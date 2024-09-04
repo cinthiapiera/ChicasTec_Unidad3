6ca6bb417b505358203497f22c2dea6c3c1bc8a3
Cinthiapiera <cinthia.maldonado@upch.pe>
2024-09-03 21:54:56 -0500

    sesion 44 terminada

--- Semana9/sesion44/script.js
+++ Semana9/sesion44/script.js
@@ -37,47 +37,73 @@
 // console.log(datos.size());
 // datos.print();
 
-class Queue{
-    constructor(){
-        this.queue=[];
-    }
 
-    enqueue(elemento){
-        this.queue.push(elemento);
-        return this.queue;
-    }
+// class Queue{
+//     constructor(){
+//         this.queue=[];
+//     }
 
-    dequeue(){
-        return this.queue.shift();
-    }
+//     enqueue(elemento){
+//         this.queue.push(elemento);
+//         return this.queue;
+//     }
 
-    peek(){
-        return this.queue[0];
-    }
+//     dequeue(){
+//         return this.queue.shift();
+//     }
 
-    size(){
-        return this.queue.length;
-    }
+//     peek(){
+//         return this.queue[0];
+//     }
 
-    isEmpty() {
-        return this.queue.length === 0;
-    }
+//     size(){
+//         return this.queue.length;
+//     }
 
-    print(){
-        console.log(this.queue);
+//     isEmpty() {
+//         return this.queue.length === 0;
+//     }
+
+//     print(){
+//         console.log(this.queue);
+//     }
+// }
+
+// const datos2 = new Queue();
+// console.log(datos2.size());
+// console.log(datos2.isEmpty());
+// console.log(datos2.enqueue('koala'));
+// console.log(datos2.enqueue('gato'));
+// console.log(datos2.enqueue('tigre'));
+// console.log(datos2.enqueue('cuy'));
+// console.log(datos2.enqueue('perro'));
+// console.log(datos2.peek());
+// console.log(datos2.dequeue());
+// datos2.print();
+// console.log(datos2.isEmpty());
+
+
+class TreeNode{
+    constructor(node){
+        this.node = node;
+        this.descendiente=[];
     }
 }
 
-const datos2 = new Queue();
-console.log(datos2.size());
-console.log(datos2.isEmpty());
-console.log(datos2.enqueue('koala'));
-console.log(datos2.enqueue('gato'));
-console.log(datos2.enqueue('tigre'));
-console.log(datos2.enqueue('cuy'));
-console.log(datos2.enqueue('perro'));
-console.log(datos2.peek());
-console.log(datos2.dequeue());
-datos2.print();
-console.log(datos2.isEmpty());
+const a = new TreeNode(10000);
+const b = new TreeNode(999);
+const c = new TreeNode(80);
+const d = new TreeNode(50);
+const e = new TreeNode(4);
+const f = new TreeNode(8);
+const g = new TreeNode(1);
 
+a.descendiente.push(b);
+b.descendiente.push(c);
+b.descendiente.push(d);
+c.descendiente.push(e);
+c.descendiente.push(f);
+c.descendiente.push(g);
+console.log(a);
+console.log(b);
+console.log(c);
